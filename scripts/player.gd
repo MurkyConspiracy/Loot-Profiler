370cc93003cd49ca079a8814f2b25c5510a1099b
MurkyConspiracy <nickreeder43@gmail.com>
2025-08-15 22:03:47 -0500

    Added basic outline for level 1

---
+++ scripts/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+const SPEED = 80.0
+
+
+func _physics_process(delta: float) -> void:
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var xdirection := Input.get_axis("ui_left", "ui_right")
+	if xdirection:
+		velocity.x = xdirection * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	var ydirection := Input.get_axis("ui_up", "ui_down")
+	if ydirection:
+		velocity.y = ydirection * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
