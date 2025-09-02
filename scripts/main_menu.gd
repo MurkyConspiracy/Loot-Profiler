05da621508f96ec8a38f772c931fcd371e912a08
MurkyConspiracy <nickreeder43@gmail.com>
2025-09-01 22:40:08 -0500

    Working on basic UI function
    Added index based button detection

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,14 +1,25 @@
 extends Control
 
 @onready var play_button: Button = $"CenterContainer/VBoxContainer/Play Button"
-
+@onready var settings_button: Button = $"CenterContainer/VBoxContainer/Settings Button"
+@onready var exit_button: Button = $"CenterContainer/VBoxContainer/Exit Button"
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventScreenTouch and event.pressed:
-		var global_touch_pos = event.position
-		var local_pos = global_touch_pos - play_button.global_position
-		if Rect2(Vector2.ZERO, play_button.size).has_point(local_pos):
-			_on_play_button_pressed()
+		match get_button_index(event.position):
+			1:
+				_on_play_button_pressed()
+			3:
+				get_tree().quit(0)
 			
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
+
+func get_button_index(eventPosition: Vector2):
+	
+	if Rect2(Vector2.ZERO, play_button.size).has_point(eventPosition - play_button.global_position):
+		return 1
+	elif Rect2(Vector2.ZERO, settings_button.size).has_point(eventPosition - settings_button.global_position):
+		return 2
+	elif Rect2(Vector2.ZERO, exit_button.size).has_point(eventPosition - exit_button.global_position):
+		return 3
--- scripts/player_view.gd
+++ scripts/player_view.gd
@@ -1,6 +1,11 @@
 extends CanvasLayer
-@onready var health_value: Label = $PanelContainer/HBoxContainer/HealthContainer/HealthValue
-@onready var health_graphic: TextureProgressBar = $PanelContainer/HBoxContainer/HealthContainer/HealthGraphic
+@onready var health_graphic: TextureProgressBar = $MarginContainer/VBoxContainer/PanelContainer/HBoxContainer/HealthContainer/HealthGraphic
+@onready var health_value: Label = $MarginContainer/VBoxContainer/PanelContainer/HBoxContainer/HealthContainer/HealthValue
+
+@onready var margin_container: MarginContainer = $MarginContainer
+@onready var skill_button_1: Button = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/SkillButton1
+@onready var skill_button_2: Button = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/SkillButton2
+@onready var skill_button_3: Button = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/SkillButton3
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,6 +15,27 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	health_value.text = str(DataHandler.player_character_health)
-	health_graphic.value = DataHandler.player_character_health
+	if(DataHandler.is_node_ready() && DataHandler.player_character_health != null):
+		health_value.text = str(DataHandler.player_character_health)
+		health_graphic.value = DataHandler.player_character_health
 	
+func _input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch and event.pressed:
+		match get_button_index(event.position):
+			1:
+				print("Skill Button 1!")
+				return
+			2:
+				print("Skill Button 2!")
+				return
+			3:
+				print("Skill Button 3!")
+				return
+	
+func get_button_index(eventPosition: Vector2):
+	if Rect2(Vector2.ZERO, skill_button_1.size).has_point(eventPosition - skill_button_1.global_position):
+		return 1
+	elif Rect2(Vector2.ZERO, skill_button_2.size).has_point(eventPosition - skill_button_2.global_position):
+		return 2
+	elif Rect2(Vector2.ZERO, skill_button_3.size).has_point(eventPosition - skill_button_3.global_position):
+		return 3
