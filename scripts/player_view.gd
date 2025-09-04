749f95565bb6f0a440c6125e0f49ed10efab6aaf
MurkyConspiracy <nickreeder43@gmail.com>
2025-09-03 23:22:18 -0500

    Added a Title Bar to PlayerView

--- scripts/player_view.gd
+++ scripts/player_view.gd
@@ -1,16 +1,23 @@
 extends CanvasLayer
-@onready var health_graphic: TextureProgressBar = $MarginContainer/VBoxContainer/PanelContainer/HBoxContainer/HealthContainer/HealthGraphic
-@onready var health_value: Label = $MarginContainer/VBoxContainer/PanelContainer/HBoxContainer/HealthContainer/HealthValue
+@onready var health_graphic: TextureProgressBar = %HealthGraphic
+@onready var health_value: Label = %HealthValue
+@onready var health_label: Label = %HealthLabel
 
 @onready var margin_container: MarginContainer = $MarginContainer
-@onready var skill_button_1: Button = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/SkillButton1
-@onready var skill_button_2: Button = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/SkillButton2
-@onready var skill_button_3: Button = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/SkillButton3
+@onready var skill_button_1: Button = %SkillButton1
+@onready var skill_button_2: Button = %SkillButton2
+@onready var skill_button_3: Button = %SkillButton3
+
+@onready var level_name_label: Label = %LevelNameLabel
+
+@onready var inventory_button: Button = %InventoryButton
+@onready var menu_button: Button = %MenuButton
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	level_name_label.text = get_tree().current_scene.name
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -31,6 +38,10 @@ func _input(event: InputEvent) -> void:
 			3:
 				print("Skill Button 3!")
 				return
+			4:
+				load_inventory()
+			5:
+				load_menu()
 	
 func get_button_index(eventPosition: Vector2):
 	if Rect2(Vector2.ZERO, skill_button_1.size).has_point(eventPosition - skill_button_1.global_position):
@@ -39,3 +50,14 @@ func get_button_index(eventPosition: Vector2):
 		return 2
 	elif Rect2(Vector2.ZERO, skill_button_3.size).has_point(eventPosition - skill_button_3.global_position):
 		return 3
+	elif Rect2(Vector2.ZERO, inventory_button.size).has_point(eventPosition - inventory_button.global_position):
+		return 4
+	elif Rect2(Vector2.ZERO, menu_button.size).has_point(eventPosition - menu_button.global_position):
+		return 5
+
+func load_inventory():
+	print("Inventory!")
+	
+	
+func load_menu():
+	print("Menu!")
